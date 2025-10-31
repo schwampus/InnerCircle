9fcfcb5faa2e7554bdf0b92954ef52cd9c69838e
Olha Fedchenko <ofedchen@gmail.com>
2025-10-31 21:45:58 +0100

    add userCircle route  get request

--- backend/routes/userCircle.js
+++ backend/routes/userCircle.js
@@ -3,17 +3,21 @@ import * as db from '../db/index.js';
 
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {
+router.get('/:users_id', async (req, res, next) => {
   try {
-    const result = await db.query('SELECT * FROM userCircle')
+    const result = await db.query('SELECT uc.uc_id, uc.uc_circle_tier, c.circle_name, c.circle_avatar FROM userCircle uc LEFT JOIN circle c ON uc.uc_circle_id = c.circle_id WHERE uc.uc_user_id = $1', [req.params.users_id])
     res.send(result.rows)
   } catch (err) {
-    console.error('Error fetching user circles', err)
+    console.log('Error fetching user circles', err)
     res.status(500).json({
       error: 'Failed to fetch user circles',
-      message: error.message
+      message: err.message
     })
   }
+})
+
+router.post('/', async (req, res) => {
+
 })
 
 export default router;
