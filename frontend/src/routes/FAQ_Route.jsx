99f1db0c4d11a11fd3bf6a953e071cf52ba5b847
Hampus Jerkfelt <hampus.jerkfelt@gmail.com>
2025-11-04 17:30:10 +0100

    Add mobile navigation menu to header component, new routes temp setup

---
+++ frontend/src/routes/FAQ_Route.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom'
+import { Button } from "@mui/joy";
+
+export default function Faq() {
+  return (<>
+    <div id="wrapper">
+   
+  
+      
+
+
+    <div className="flex text-6xl font-black justify-center items-center text-center">
+      THIS IS Faq
+    </div>
+    
+    <div className="bg-(--purple-dark) w-full h-64 text-(--orange-main) px-8 py-10">
+      <h2 className="font-bold">More about the circles:</h2>
+    </div>
+    </div></>
+  )
+}
+
+
