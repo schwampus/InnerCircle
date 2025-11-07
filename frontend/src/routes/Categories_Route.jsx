0223717127b8de3b3c3f2be0f116774e9954ea81
Hampus Jerkfelt <hampus.jerkfelt@gmail.com>
2025-11-07 14:27:40 +0100

    basic layuot start

--- frontend/src/routes/Categories_Route.jsx
+++ frontend/src/routes/Categories_Route.jsx
@@ -1,23 +1,46 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 import { Button } from "@mui/joy";
 
 export default function Categories() {
-  return (<>
-    <div id="wrapper">
-   
-  
-      
+	return (
+		<>
+			<div id="wrapper">
+				<div className="flex text-3xl my-4 font-climate justify-center items-center text-center">
+					CATEGORIES
+				</div>
+				<div className="flex flex-wrap justify-center p-8 gap-8 my-8">
+					<img
+						src="https://images.unsplash.com/photo-1455264646464-fb8b45ab4c57?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format"
+						className="w-34 h-34 rounded-full mx-auto mb-4"
+					/>
+					<img
+						src="https://images.unsplash.com/photo-1502680390469-be75c86b636f?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+						className="w-34 h-34 rounded-full mx-auto mb-4"
+					/>
+					<img
+						src="https://images.unsplash.com/photo-1455264646464-fb8b45ab4c57?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format"
+						className="w-34 h-34 rounded-full mx-auto mb-4"
+					/>
+					<img
+						src="https://i.imgur.com/KwqolLd.png"
+						className="w-34 h-34 rounded-full mx-auto mb-4"
+					/>
+					<img
+						src="https://images.unsplash.com/photo-1673552435410-d114bb408e91?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+						className="w-34 h-34 rounded-full mx-auto mb-4"
+					/>
 
+					<img
+						src="https://images.pexels.com/photos/24742693/pexels-photo-24742693.jpeg"
+						className="w-34 h-34 rounded-full mx-auto mb-4"
+					/>
+					<img src="" className="w-44 h-44 rounded-full mx-auto mb-4" />
+				</div>
 
-    <div className="flex text-6xl font-black justify-center items-center text-center">
-      THIS IS Categories
-    </div>
-    
-    <div className="bg-(--purple-dark) w-full h-64 text-(--orange-main) px-8 py-10">
-      <h2 className="font-bold">More about the circles:</h2>
-    </div>
-    </div></>
-  )
+				<div className="bg-(--purple-dark) w-full h-64 text-(--orange-main) px-8 py-10">
+					<h2 className="font-bold">More about the circles:</h2>
+				</div>
+			</div>
+		</>
+	);
 }
-
-
