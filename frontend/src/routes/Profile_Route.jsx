34d31cacc0e275ce5ddeb2cb44187f585a8b3f8c
Hampus Jerkfelt <hampus.jerkfelt@gmail.com>
2025-11-11 16:49:56 +0100

    conditional render of circles

--- frontend/src/routes/Profile_Route.jsx
+++ frontend/src/routes/Profile_Route.jsx
@@ -1,4 +1,5 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
 import {
 	Accordion,
 	AccordionDetails,
@@ -8,19 +9,29 @@ import {
 } from "@mui/joy";
 import Avatar from "../components/Avatar";
 import { useUser } from "../hooks/useUser";
+
 import { useState } from "react";
 import { useEffect } from "react";
+import Divider from "@mui/joy/Divider";
+import DialogTitle from "@mui/joy/DialogTitle";
+import DialogContent from "@mui/joy/DialogContent";
+import DialogActions from "@mui/joy/DialogActions";
+import Modal from "@mui/joy/Modal";
+import ModalDialog from "@mui/joy/ModalDialog";
+import WarningRoundedIcon from "@mui/icons-material/WarningRounded";
 
 export default function Profile() {
-	const { userId } = useUser();
+	const { userId, logout } = useUser();
 
 	const [userData, setUserData] = useState(null);
 	const [newUsername, setNewUsername] = useState("");
 	const [newUserEmail, setNewUserEmail] = useState("");
 	const [newPaymentMethod, setNewPaymentMethod] = useState("");
 	const [hasChanges, setHasChanges] = useState(false);
+	const [open, setOpen] = useState(false);
 
-	const [myCircles, setMyCircles] = useState("");
+	const [myCircles, setMyCircles] = useState(null);
+	const navigate = useNavigate();
 
 	useEffect(() => {
 		fetch(`/api/users/${userId}`)
@@ -40,7 +51,7 @@ export default function Profile() {
 			.then((data) => {
 				setMyCircles(data);
 			});
-	}, []);
+	}, [userId]);
 
 	useEffect(() => {
 		if (!userData) return;
@@ -87,10 +98,33 @@ export default function Profile() {
 			alert("Error saving changes");
 		}
 	}
+	async function handleDeleteUser() {
+		try {
+			const response = await fetch(`/api/users/${userId}`, {
+				method: "DELETE",
+				headers: {
+					"Content-Type": "application/json",
+				},
+			});
+
+			if (response.ok) {
+				logout();
+				setOpen(false);
+				navigate("/");
+
+				console.log("Användare borttagen");
+			} else {
+				alert("Nåt gick fel vid borttagning");
+			}
+		} catch (error) {
+			console.error("Error saving changes:", error);
+			alert("Error saving changes");
+		}
+	}
 
 	return (
 		<>
-			<div id="wrapper">
+			<div className="bg-(--purple-dark) min-h-screen">
 				<div
 					id="profile-hero"
 					className="flex flex-col self-start items-center justify-around text-2xl font-black w-full bg-(--purple-main) text-(--orange-lighter)  text-center"
@@ -101,7 +135,7 @@ export default function Profile() {
 							className="h-40 w-40"
 							src="https://avatar.iran.liara.run/public/ 45"
 						/>
-						<svg
+						{/* removed change avatar functionality for now		<svg
 							className="ml-[-22px]"
 							xmlns="http://www.w3.org/2000/svg"
 							width="24"
@@ -116,7 +150,7 @@ export default function Profile() {
 						>
 							<path d="M13.997 4a2 2 0 0 1 1.76 1.05l.486.9A2 2 0 0 0 18.003 7H20a2 2 0 0 1 2 2v9a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V9a2 2 0 0 1 2-2h1.997a2 2 0 0 0 1.759-1.048l.489-.904A2 2 0 0 1 10.004 4z" />
 							<circle cx="12" cy="13" r="3" />
-						</svg>
+						</svg> */}
 					</div>
 				</div>
 
@@ -149,7 +183,7 @@ export default function Profile() {
 									/>
 								</AccordionDetails>
 								<AccordionDetails>
-									<label className="profile-label">Change you email:</label>
+									<label className="profile-label">Change your email:</label>
 									<input
 										className="profile-input"
 										placeholder={userData?.users_email}
@@ -175,7 +209,7 @@ export default function Profile() {
 									Change Payment Method
 								</AccordionSummary>
 								<AccordionDetails>
-									<label className="profile-label">Change you email:</label>
+									<label className="profile-label">Select new method:</label>
 									<select
 										className="profile-input"
 										value={newPaymentMethod}
@@ -187,12 +221,68 @@ export default function Profile() {
 									</select>
 								</AccordionDetails>
 							</Accordion>
+							<Accordion
+								sx={{
+									"&.Mui-expanded": {
+										backgroundColor: "var(--orange-white)",
+										color: "var(--orange-lighter)",
+									},
+								}}
+							>
+								<AccordionSummary
+									sx={{
+										color: "var(--orange-main)",
+										backgroundColor: "var(--purple-dark)",
+									}}
+								>
+									Remove Your Account
+								</AccordionSummary>
+								<AccordionDetails>
+									<div className="flex justify-center m-4">
+										<Button
+											onClick={() => setOpen(true)}
+											color="danger"
+											variant="solid"
+										>
+											Remove Account
+										</Button>
+										<Modal open={open} onClose={() => setOpen(false)}>
+											<ModalDialog variant="outlined" role="alertdialog">
+												<DialogTitle>
+													<WarningRoundedIcon />
+													Are you sure?
+												</DialogTitle>
+												<Divider />
+												<DialogContent>
+													Are you sure you want to remove your account?
+												</DialogContent>
+												<DialogActions>
+													<Button
+														variant="solid"
+														color="danger"
+														onClick={handleDeleteUser}
+													>
+														Remove Account
+													</Button>
+													<Button
+														variant="plain"
+														color="neutral"
+														onClick={() => setOpen(false)}
+													>
+														Cancel
+													</Button>
+												</DialogActions>
+											</ModalDialog>
+										</Modal>
+									</div>
+								</AccordionDetails>
+							</Accordion>
 							{hasChanges && (
 								<div className="flex justify-center m-4">
 									<Button
 										onClick={handleSaveChanges}
-										color="primary"
-										variant="solid"
+										color="secondary"
+										variant="outlined"
 									>
 										Save Changes
 									</Button>
@@ -202,22 +292,34 @@ export default function Profile() {
 					)}
 				</div>
 
-				<div className="flex flex-col justify-center items-center bg-(--purple-dark) w-full text-(--orange-main) px-8 py-10">
-					<h1 className="text-4xl text-(--orange-main) font-bold font-kanit pt-2 mb-8">
+				<div className="flex flex-col justify-center items-center bg-(--purple-dark) w-full  text-(--orange-main) px-8 py-10">
+					<h1 className="text-4xl text-(--orange-light) font-bold font-kanit pt-2 mb-8">
 						{" "}
 						YOUR CIRCLES
 					</h1>
 					<div className="flex flex-wrap justify-center gap-8 px-4 sm:px-20 ">
-						{myCircles &&
+						{myCircles && myCircles.length > 0 ? (
 							myCircles.map((item) => {
 								return (
 									<Avatar
 										tierColor={item.uc_circle_tier}
 										src={item.circle_avatar}
 										name={item.circle_name}
+										key={item.uc_id}
 									/>
 								);
-							})}
+							})
+						) : (
+							<div className="flex flex-col justify-center ">
+								<p className="font-semibold pb-4 text-(--purple-white)">
+									{" "}
+									You don't have any circles yet.
+								</p>
+								<Button variant="solid" color="secondary">
+									<Link to="/categories">EXPLORE CIRCLES</Link>
+								</Button>
+							</div>
+						)}
 					</div>
 				</div>
 			</div>
