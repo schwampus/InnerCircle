0ea0335c03a61a47db284b3477290941052b2045
Hampus Jerkfelt <hampus.jerkfelt@gmail.com>
2025-11-13 02:22:08 +0100

    logo in header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -7,100 +7,107 @@ import { useUser } from "../hooks/useUser";
 import AuthModal from "./AuthModal";
 
 export default function Header() {
-  const { userId, logout } = useUser();
-  const [open, setOpen] = useState(false);
+	const { userId, logout } = useUser();
+	const [open, setOpen] = useState(false);
 
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const handleLinkClick = () => {
-    setOpen(false);
-  };
+	const handleLinkClick = () => {
+		setOpen(false);
+	};
 
-  const Logout = () => {
-    logout();
-    setOpen(false);
-    navigate("/");
-  };
+	const Logout = () => {
+		logout();
+		setOpen(false);
+		navigate("/");
+	};
 
-  return (
-    <>
-      <div className="flex justify-between items-center px-6 bg-(--purple-white) h-18">
-        <div>
-          <IconButton
-            variant="outlined"
-            color="primary"
-            onClick={() => setOpen(true)}
-          >
-            <MenuOutlined />
-          </IconButton>
+	return (
+		<>
+			<div className="grid grid-cols-3 items-center px-6 bg-(--purple-white) h-18">
+				<div>
+					<IconButton
+						variant="outlined"
+						color="primary"
+						onClick={() => setOpen(true)}
+					>
+						<MenuOutlined />
+					</IconButton>
 
-          <Drawer open={open} onClose={() => setOpen(false)}>
-            <Box
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                gap: 0.5,
-                ml: "auto",
-                mt: 1,
-                mr: 2,
-              }}
-            >
-              <ModalClose id="close-icon" sx={{ position: "initial" }} />
-            </Box>
-            <ul className="flex text-xl bg-(--purple-dark) h-full flex-col text-center gap-12 pt-18 mt-[-40px] text-(--orange-main) font-kanit font-bold">
-              <li>
-                <Link to="/" onClick={handleLinkClick}>
-                  HOME
-                </Link>
-              </li>
-              <li>
-                <Link to="/feed" onClick={handleLinkClick}>
-                  FEED
-                </Link>
-              </li>
-              {userId && (
-                <li>
-                  <Link to="/profile" onClick={handleLinkClick}>
-                    PROFILE
-                  </Link>
-                </li>
-              )}
-              <li>
-                <Link to="/categories" onClick={handleLinkClick}>
-                  CATEGORIES
-                </Link>
-              </li>
-              <li>
-                <Link to="/about" onClick={handleLinkClick}>
-                  ABOUT US
-                </Link>
-              </li>
-              <li>
-                <Link to="/faq" onClick={handleLinkClick}>
-                  FAQ
-                </Link>
-              </li>
-              <li>
-                {userId && (
-                  <Button
-                    onClick={Logout}
-                    variant="outlined"
-                    color="secondary"
-                    sx={{ mt: 20 }}
-                  >
-                    LOG OUT
-                  </Button>
-                )}
-              </li>
-            </ul>
+					<Drawer open={open} onClose={() => setOpen(false)}>
+						<Box
+							sx={{
+								display: "flex",
+								alignItems: "center",
+								gap: 0.5,
+								ml: "auto",
+								mt: 1,
+								mr: 2,
+							}}
+						>
+							<ModalClose id="close-icon" sx={{ position: "initial" }} />
+						</Box>
+						<ul className="flex text-xl bg-(--purple-dark) h-full flex-col text-center gap-12 pt-18 mt-[-40px] text-(--orange-main) font-kanit font-bold">
+							<li>
+								<Link to="/" onClick={handleLinkClick}>
+									HOME
+								</Link>
+							</li>
+							<li>
+								<Link to="/feed" onClick={handleLinkClick}>
+									FEED
+								</Link>
+							</li>
+							{userId && (
+								<li>
+									<Link to="/profile" onClick={handleLinkClick}>
+										PROFILE
+									</Link>
+								</li>
+							)}
+							<li>
+								<Link to="/categories" onClick={handleLinkClick}>
+									CATEGORIES
+								</Link>
+							</li>
+							<li>
+								<Link to="/about" onClick={handleLinkClick}>
+									ABOUT US
+								</Link>
+							</li>
+							<li>
+								<Link to="/faq" onClick={handleLinkClick}>
+									FAQ
+								</Link>
+							</li>
+							<li>
+								{userId && (
+									<Button
+										onClick={Logout}
+										variant="outlined"
+										color="secondary"
+										sx={{ mt: 20 }}
+									>
+										LOG OUT
+									</Button>
+								)}
+							</li>
+						</ul>
 
-            <p className="pb-12 text-sm text-center bg-(--purple-dark) text-(--purple-lighter)">
-              all rights reserved © FEDFELT
-            </p>
-          </Drawer>
-        </div>
-        {!userId && <AuthModal modalType={"login"} />}
-      </div>
-    </>
-  );
+						<p className="pb-12 text-sm text-center bg-(--purple-dark) text-(--purple-lighter)">
+							all rights reserved © FEDFELT
+						</p>
+					</Drawer>
+				</div>
+
+				<div className="flex justify-center">
+					<img src="/favicon.svg" className="w-12 h-12" alt="Logo" />
+				</div>
+
+				<div className="flex justify-end">
+					{!userId && <AuthModal modalType={"login"} />}
+				</div>
+			</div>
+		</>
+	);
 }
--- frontend/src/routes/Home_Route.jsx
+++ frontend/src/routes/Home_Route.jsx
@@ -4,40 +4,38 @@ import { Link } from "react-router-dom";
 import AuthModal from "../components/AuthModal";
 
 export default function Home() {
-  const { userId } = useUser();
+	const { userId } = useUser();
 
-  return (
-    <>
-      <div id="wrapper">
-        <div
-          id="herocontent"
-          className="bg-[url('/ic-temp.webp')] 
+	return (
+		<>
+			<div id="wrapper">
+				<div
+					id="herocontent"
+					className="bg-[url('/ic-temp.webp')] 
     sm:bg-[url('/ic-temp-sm.webp')] bg-contain bg-center bg-no-repeat
     h-[30dvh] sm:h-[30dvh]
     w-full
     flex flex-col justify-center items-center text-center
     overflow-hidden"
-        >
-          <h1 className="text-6xl font-climate text-(--orange-main) ">
-            INNER
-          </h1>
-          <h1 className="text-6xl font-climate text-(--purple-main) ">
-            CIRCLE
-          </h1>
-        </div>
-        <div className="flex bg-(--purple-lighter) w-full justify-around gap-2 px-8 py-8">
-          <Button variant="solid" color="secondary">
-            <Link to="/categories">EXPLORE CIRCLES</Link>
-          </Button>
-          {!userId && <AuthModal modalType={"signup"} />}
-        </div>
-        <div className="bg-(--purple-dark) w-full h-64 text-(--orange-main) px-8 py-10">
-          <h2 className="font-[400] text-2xl text-center px-8">
-            Be first to know about your idols upcoming adventures ❤️‍🔥
-          </h2>
-          {/* <img src="" alt="" /> TODO: BLUR UNBLUR POST GIF */}
-        </div>
-      </div>
-    </>
-  );
+				>
+					<h1 className="text-6xl font-climate text-(--orange-main) ">INNER</h1>
+					<h1 className="text-6xl font-climate text-(--purple-main) ">
+						CIRCLE
+					</h1>
+				</div>
+				<div className="flex bg-(--purple-lighter) w-full justify-around gap-2 px-8 py-8">
+					<Button variant="solid" color="secondary">
+						<Link to="/categories">EXPLORE CIRCLES</Link>
+					</Button>
+					{!userId && <AuthModal modalType={"signup"} />}
+				</div>
+				<div className="bg-(--purple-dark) w-full h-64 text-(--orange-main) px-8 py-10">
+					<h2 className="font-[400] text-2xl text-center px-8">
+						Be first to know about your idols upcoming adventures ❤️‍🔥
+					</h2>
+					{/* <img src="" alt="" /> TODO: BLUR UNBLUR POST GIF */}
+				</div>
+			</div>
+		</>
+	);
 }
