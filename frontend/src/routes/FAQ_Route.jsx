bac8d81e4947b59fcd3bd346ec2a973b9ebf39f7
Hampus Jerkfelt <hampus.jerkfelt@gmail.com>
2025-11-13 02:39:29 +0100

    faq

--- frontend/src/routes/FAQ_Route.jsx
+++ frontend/src/routes/FAQ_Route.jsx
@@ -1,23 +1,30 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 import { Button } from "@mui/joy";
 
 export default function Faq() {
-  return (<>
-    <div id="wrapper">
-   
-  
-      
+	return (
+		<>
+			<div idgit="wrapper">
+				<div className="bg-(--orange-white) flex flex-col text-6xl font-black justify-center items-center text-center">
+					<h1 className="font-kanit font-bold text-(--orange-main) my-8">
+						{" "}
+						F.A.Q
+					</h1>
+					<img src="/logo-white.png" className="h-30 w-30  mb-4" />
+				</div>
 
-
-    <div className="flex text-6xl font-black justify-center items-center text-center">
-      THIS IS Faq
-    </div>
-    
-    <div className="bg-(--purple-dark) w-full h-64 text-(--orange-main) px-8 py-10">
-      <h2 className="font-bold">More about the circles:</h2>
-    </div>
-    </div></>
-  )
+				<div className="bg-(--purple-dark) w-full h-64 text-(--orange-main) px-8 py-10 ">
+					<p className="py-2">
+						An athelete has a feed where they can post annoncements. A fan can
+						find atheletes and subscribe to them with a monthly monetary “gift”.
+					</p>
+					<p className="py-2">
+						There are 3 membership tiers: bronze, silver and gold. Only fans of
+						an idol can view exclusive content and fans see the content
+						available for the respective tier and lower.{" "}
+					</p>
+				</div>
+			</div>
+		</>
+	);
 }
-
-
